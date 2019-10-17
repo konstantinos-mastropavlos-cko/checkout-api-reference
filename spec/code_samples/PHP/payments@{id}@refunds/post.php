19463bd9dad0a14428ae6bfae53e0c5984d082ea
francesca-gaunard-cko <43998723+francesca-gaunard-cko@users.noreply.github.com>
2019-10-17 17:27:30 +0100

    Update post.php

--- spec/code_samples/PHP/payments@{id}@refunds/post.php
+++ spec/code_samples/PHP/payments@{id}@refunds/post.php
@@ -3,7 +3,7 @@
 $checkout = new CheckoutApi('your secret key');
 $paymentID = 'pay_y3oqhf46pyzuxjbcn2giaqnb44';
 
-// Full Capture
+// Full refund
 $refund = new Refund($paymentID);
 
 // Or partial refund
